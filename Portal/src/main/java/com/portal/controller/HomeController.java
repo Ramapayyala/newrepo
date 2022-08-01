7870bbab286d6ae6b1bcbd746ff09ee14d6f54e0
Ramapayyala <ramakrishna.payyala@gmail.com>
2022-08-01 09:55:16 +0530

    first commit

---
+++ Portal/src/main/java/com/portal/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+	
+	
+	@GetMapping("/job")
+	public String job() {
+		
+		return "job";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "login";
+	}
+	
+	@GetMapping("/admin/home")
+	public String adminindex() {
+		
+		return "admin/index";
+	}
+	
+}
